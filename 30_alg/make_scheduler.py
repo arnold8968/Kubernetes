fc32726e89b4347107a44cbfb8c6aa341fba45ab
James Yan <840520333@qq.com>
2020-01-21 10:48:49 -0500

    Update make_scheduler.py

--- 30_alg/make_scheduler.py
+++ 30_alg/make_scheduler.py
@@ -3,6 +3,8 @@ import os
 import logging
 import sys
 import time
+import numpy as np
+import pandas as pd
 
 logger = logging.getLogger(__name__)
 logger.setLevel(level = logging.INFO)
@@ -13,17 +15,17 @@ handler.setFormatter(formatter)
 logger.addHandler(handler)
 
 def get_pool():
-    f = open("pool/pool.txt","r")
+    f = open("pool/pool.txt","r")       # get the name for deeplearning methods, such as tf_birnn, tf_drnn 
     lines = f.read().split("\n")
     lines = filter(lambda s:len(s)>0,lines)
     f.close()
     return lines
 
 def select_one(pool):
-    return random.sample(pool,1)[0]
+    return random.sample(pool,1)[0] # random chose one method
 
 def create_yaml(name,job,scheduler):
-    f_pool = open("pool/"+name+".yaml","r")
+    f_pool = open("pool/"+name+".yaml","r")# sample yaml method, replace the name and schedulerName
     conf = f_pool.read()
     conf = conf.replace("Needtoplaced",job+"-"+name)
     conf = conf.replace("scheduler-need-to-be-replaced",scheduler)
@@ -33,30 +35,45 @@ def create_yaml(name,job,scheduler):
     f_output.close()
 
 
-def create_scheduler():
-    logger.info("create a new scheduler")
-    f = open("scheduler.csv","w")
-    f.write("yaml,seconds,name\n")
-    f.close()
 
-def add_scheduler(name,job,time):
-    second = random.randint(0,time)
-    with open("scheduler.csv","a") as f:
-        f.write(job+".yaml,"+str(second)+","+job+"-"+name+"\n")
-        logger.info("add "+job+"-"+name+",submitted on "+str(second))
-        f.close()
 
+#def add_scheduler(name,job,time): # name: tf_birnn, job: job1, time
+#    second = random.randint(0,time)
+#    with open("scheduler.csv","a") as f:
+#        f.write(job+".yaml,"+str(second)+","+job+"-"+name+"\n")
+#        logger.info("add "+job+"-"+name+",submitted on "+str(second))
+#        f.close()
+
+
+#def create_scheduler():
+#    logger.info("create a new scheduler")
+#    f = open("scheduler.csv","w")
+#    f.write("yaml,seconds,name\n")
+#    f.close()
+#    
+    
+    
 def main(job_num,time,scheduler):
     os.popen("rm *.yaml")
     logger.info("delete all previous yamls")
-    create_scheduler()
+#    create_scheduler()
     pool = get_pool()
+    job_list = []
+    name_list = []
     for i in range(1,job_num+1):
         job = "job"+str(i)
-        name = select_one(pool)
+        name = select_one(pool) # name: 
         create_yaml(name,job,scheduler)
         logger.info("create a "+name+" yaml named "+job+".yaml")
-        add_scheduler(name,job,time)
+        job_list.append(job)
+        name_list.append(name)
+#        add_scheduler(name,job,time)
+    logger.info("create a new scheduler")
+    second = list(np.random.randint(time, size = job_num))
+    second.sort()
+    output = {'yaml':job_list,'seconds':second,'name':name_list}
+    df = pd.DataFrame(output)
+    df.to_csv('scheduler.csv')
     logger.info("successful created yamls and scheduler")
 
 
